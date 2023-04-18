33768e15d37c641ddaefcb14280d8f1af1202ddd
sako00 <sanna.erv@hotmail.com>
2023-04-18 10:58:53 +0300

    Muutoksia addartist

--- add_artist_info.php
+++ add_artist_info.php
@@ -6,25 +6,29 @@
 require "dbconnection.php";
 $dbcon = createDbConnection();
 
-$Name = strip_tags($_POST["artistname"]);
-$Title = strip_tags($_POST["albumstitle"]);
+// Get the JSON data
+$body = file_get_contents("php://input");
+$data = json_decode($body);
 
+// Get the artist information
+$artistName = $data->artist_name;
+$artistSql = "INSERT INTO artists (Name) VALUES (?)";
+$artistStmt = $dbcon->prepare($artistSql);
+$artistStmt->execute([$artistName]);
 
-// Insert the artist
-$sql = "INSERT INTO artists (Name) VALUES (?)";
-$statement = $dbcon->prepare($sql);
-$statement->execute(array($Name));
+// Get the album information
+$albumTitle = $data->album_title;
+$albumArtistId = $dbcon->lastInsertId(); // The ID of the artist we just inserted
+$albumSql = "INSERT INTO albums (Title, ArtistId) VALUES (?, ?)";
+$albumStmt = $dbcon->prepare($albumSql);
+$albumStmt->execute([$albumTitle, $albumArtistId]);
+$albumId = $dbcon->lastInsertId(); // The ID of the album we just inserted
 
-// Get the artist ID
-$ArtistId = $dbcon->lastInsertId();
-
-// Insert the album with the artist ID as a foreign key
-$sql = "INSERT INTO albums (ArtistId,Title) VALUES (?, ?)";
-$statement = $dbcon->prepare($sql);
-$statement->execute(array($ArtistId,$Title));
-
-
-
-
-
-  
+// Get the tracks information
+$tracks = $data->tracks;
+$trackSql = "INSERT INTO tracks (Name, AlbumId, MediaTypeId, GenreId, Composer,Milliseconds,Bytes,UnitPrice) VALUES (?, ?, ?, ?, ?, ? , ? , ?)";
+$trackStmt = $dbcon->prepare($trackSql);
+foreach ($tracks as $track) {
+    $trackStmt->execute([$track->name, $albumId, 
+    $track->media_type_id, $track->genre_id, $track->composer, $track->Milliseconds, $track->Bytes, $track->Unitprice]);
+}

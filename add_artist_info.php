608d20f08c8627916e8935370a4500ddb2a9e8f7
sako00 <sanna.erv@hotmail.com>
2023-04-21 09:40:18 +0300

    addartist koodin testaus

--- add_artist_info.php
+++ add_artist_info.php
@@ -4,42 +4,22 @@
 //Kaikki tarvittavat tiedot saadaan joko POST- tai JSON-muodossa parametreina. Voit
 //aloittaa luomalla vain artistin ja albumin.
 
+//Retrieve the artist name, album title, and track names from the decoded JSON data
 require "dbconnection.php";
 $dbcon = createDbConnection();
 
-// Receive and decode the JSON data from the request body
 $body = file_get_contents("php://input");
 $data = json_decode($body);
 
-// Retrieve the artist name, album title, and track names from the decoded JSON data
-$artistName = strip_tags($data->artist);
-$albumTitle = strip_tags($data->album);
-$trackNames = $data->tracks;
 
-// Insert the new artist into the artists table
-$sql = "INSERT INTO artists (Name) VALUES (?)";
+//$sql = "INSERT INTO artists (Name) VALUES (?)";
+//$statement = $dbcon->prepare($sql);
+//foreach($data as $a){
+//$statement->execute(array($a->name));
+    
+$sql = "INSERT INTO albums (Title, ArtistId) VALUES (?, ?)";
 $statement = $dbcon->prepare($sql);
-$statement->execute(array($artistName));
+$statement->execute([$albumTitle, $artistId]);
 
-// Retrieve the artist ID from the database
-$artistId = $dbcon->lastInsertId();
-
-// Insert the new album into the albums table
-$sql = "INSERT INTO albums (Title, ArtistId) VALUES (?,?)";
-$statement = $dbcon->prepare($sql);
-$statement->execute(array($albumTitle,$artistId));
-
-// Retrieve the album ID from the database
 $albumId = $dbcon->lastInsertId();
-
-// Loop through the track names and insert each track into the tracks table
-foreach ($trackNames as $trackName) {
-    $sql = "INSERT INTO tracks (Name, AlbumId, MediaTypeId, Milliseconds, UnitPrice) VALUES (?,?,?,?,?)";
-$statement = $dbcon->prepare($sql);
-$statement->execute(array($trackName, $albumId, 1, $milliseconds, $unitPrice));
-
-}
-
-echo "New artist and album created successfully!";
-?>
 

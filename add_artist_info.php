16286cd5b0fbd04d7206680ea2d45e7258aefb12
sako00 <sanna.erv@hotmail.com>
2023-04-25 08:33:54 +0300

    artist info Koodi toimii

--- add_artist_info.php
+++ add_artist_info.php
@@ -11,51 +11,37 @@ $dbcon = createDbConnection();
 $body = file_get_contents("php://input");
 $data = json_decode($body);
 
+//Laitoin ensin artistin nimen
 
 //$sql = "INSERT INTO artists (Name) VALUES (?)";
 //$statement = $dbcon->prepare($sql);
 //foreach($data as $a){
 //$statement->execute(array($a->name));
-    
 
-// Check if all required data is present
-if (!isset($data->title) || !isset($data->artist_id) || !isset($data->trackNames) || !isset($data->mediatypeId) || !isset($data->milliseconds) || !isset($data->unitprice)) {
-        // Return an error message if any required data is missing
-        http_response_code(400);
-        echo json_encode(array("error" => "Missing required data"));
-        exit();
-    }
+// Lisätään albumi
+
+$stmt = $dbcon->prepare("INSERT INTO albums (Title, ArtistId) VALUES (?, ?)");
+$stmt->execute(array($data->title, $data->artist_id));
+$album_id = $dbcon->lastInsertId();
+
+// Lisätään kappaleet
+foreach ($data->trackNames as $trackName) {
+  $statement = $dbcon->prepare("INSERT INTO tracks (Name, AlbumId, MediaTypeId, Milliseconds, UnitPrice) VALUES (?, ?, ?, ?, ?)");
+  $statement->execute(array($trackName, $album_id, $data->mediatypeId, $data->milliseconds, $data->unitprice));
+  $track_id = $dbcon->lastInsertId();
+}
+//Postmanilla lähetetty tieto
+//{
     
-    // Insert the album into the database
-    $stmt = $dbcon->prepare("INSERT INTO albums (Title, ArtistId) VALUES (?, ?)");
-    $stmt->execute(array($data->title, $data->artist_id));
-    $album_id = $dbcon->lastInsertId();
-    
-    // Insert each track into the database
-    foreach ($data->trackNames as $trackName) {
-        $statement = $dbcon->prepare("INSERT INTO tracks (Name, AlbumId, MediaTypeId, Milliseconds, UnitPrice) VALUES (?, ?, ?, ?, ?)");
-        $statement->execute(array($trackName, $album_id, $data->mediatypeId, $data->milliseconds, $data->unitprice));
-        $track_id = $dbcon->lastInsertId();
-    }
-    
-    //{
-      //  "artistName": "Popeda",
-        //"artist_id":1,
-        //"title": "Kaasua, komisario Palmu!",
-        //"trackNames": [
-          //  "Hullun paperit",
-            //"Hän",
-            //"Odotusaika",
-            //"Kersantti Karoliina",
-            //"Vielä virtaa",
-            //"Vauhtiajot",
-            //"Sukset",
-            //"Korkeajännitystä",
-            //"Oi Mari",
-            //"Matkalla Alabamaan"
-        //],
-        //"mediatypeId":1,
-        //"milliseconds":123456,
-        //"unitprice":0.99
-    //}
-    
+  //"title": "Harasoo",
+  //"artist_id":1,
+  //"trackNames": [
+    //  "Pullaristo ihmemaassa",
+      //"Whiskey",
+      //"Olen valmis",
+      //"Matkalla alabamaan"
+  //],
+  //"mediatypeId":1,
+  //"milliseconds":123456,
+  //"unitprice":0.99
+//}
